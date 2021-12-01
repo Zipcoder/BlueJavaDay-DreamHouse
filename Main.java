480e579b40c0160f985870c2474e9dccb467f735
Kristofer <kris@youngers.org>
2021-12-01 10:06:14 -0500

    adding classes

---
+++ Main.java
@@ -0,0 +1,8 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Start Drawing!");
+    Picture p = new Picture();
+    p.draw();
+    System.out.println("Done Drawing.");
+  }
+}
